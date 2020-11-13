4c615991c986fc0eab6a5aff6c6dcddd552523e9
AnAsianGangster <AnAsianGangster@users.noreply.github.com>
2020-11-14 01:21:04 +0800

    add user controllers

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,9 +1,45 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/auth.config.js');
+
+const db = require('../models/index');
+const sequelize = require('sequelize');
+const Book = db.books;
+const User = db.user;
+
 exports.allAccess = (req, res) => {
-    res.status(200).send('Public Content.');
+    const numberOfBooks = parseInt(req.params.numberOfBooks) || 5;
+
+    Book.findAll({ order: [[sequelize.literal('RAND()')]], limit: numberOfBooks }).then((data) => {
+        res.status(200).send(data);
+    });
 };
 
 exports.userBoard = (req, res) => {
     res.status(200).send('User Content.');
+};
+
+exports.userProfile = (req, res) => {
+    const token = req.headers['x-access-token'];
+    const reviewerID = parseInt(req.params.reviewerID);
+
+    jwt.verify(token, config.secret, (err, decoded) => {
+        if (reviewerID != decoded.id) {
+            return res.status(401).send({
+                message: 'Unauthorized!',
+            });
+        } else {
+            return User.findByPk(reviewerID, { include: ['reviews'] })
+                .then((data) => {
+                    res.send(data);
+                })
+                .catch((err) => {
+                    res.status(500).send({
+                        message:
+                            err.message || 'Some error occurred while finding reviews by user.',
+                    });
+                });
+        }
+    });
 };
 
 exports.adminBoard = (req, res) => {
